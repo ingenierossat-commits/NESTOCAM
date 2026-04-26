5253f6bd86fea7390a4adf73ecba1a580062a9f5
ingenierossat-commits <ingenieros.sat@gmail.com>
2026-04-26 12:19:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'nestocam-v3';
+const ASSETS = ['/', '/nidos.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
